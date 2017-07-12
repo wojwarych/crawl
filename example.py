bf0a2c7680d27a9ab17247d39e55db54c974b651
Wojciech Warych <woj.warych@gmail.com>
2017-07-12 03:23:28 +0200

    first commit

---
+++ example.py
@@ -0,0 +1,60 @@
+from wrap_crawl import SiteInfo, TheCrawl
+
+
+if __name__ == "__main__":
+
+	#Set the crawler - starting point, robots.txt, file to write down to it, headers
+	data = SiteInfo("robot_parser.txt")
+	data.create_table("dane")
+
+	url = data.data_from_txt.readline()
+	headers = data.data_from_txt.readline()
+	robots_info = data.data_from_txt.readline()
+
+	data.write_table(headers)
+	data.check_robots(robots_info)
+	data.check_url(url)
+
+	iter_page = int(input("Set the starting page: "))
+	num_pages = int(input("Set the number of pages: "))
+	url = url[:-1]
+
+	while iter_page <= num_pages:
+		
+		urliter = (url + str(iter_page))
+
+		#Get the url and its content
+		crawler = TheCrawl(urliter)
+		#Get the specific content of a page - another links
+		crawler.get_link('href', 'a', class_ = 'product')
+		crawler.get_content(data)
+		crawler.get_details('table', class_ = 'productSpecification')
+		crawler.reformat("Dowiedz się więcej więcej ", "")
+		crawler.insert_to_reformat(" zł", "", 1, 'div', class_ = 'newPrice')
+
+		for number, _ in enumerate(crawler.reformatted):
+			
+			crawler.add_by_regex("Producent: (.+?) Kod", number, ";")
+			crawler.add_by_regex("Długość \(cm\): (.+?) Waga", number, ";")
+			crawler.add_by_regex("Waga \(g\): (.+?) Powierzchnia", number, ";")
+			crawler.add_by_regex("Powierzchnia główki \(cm2\): (.+?) Balans", number, ";")
+			crawler.add_by_regex("Balans \(mm\): (.+?) Sztywność", number, ";")
+			crawler.add_by_regex("Sztywność \(RA\): (.+?) Układ", number, ";")
+			crawler.add_by_regex("Układ strun: (.+?) Profil", number, ";")
+			crawler.add_by_regex("Profil ramy \(mm\): (.+?) ", number, ";")
+			crawler.add_by_regex("([0-9]{3,4}?,[0-9]{2})", number, "\n")
+
+			crawler.product_spec.append(crawler.text)
+
+			crawler.clear_text()
+
+		for index, _ in enumerate(crawler.product_spec):
+
+			data.write_table(crawler.product_spec[index])
+		
+		print(crawler.product_spec)
+
+		iter_page +=1
+
+	#Close csv file
+	data.close_file()
---
+++ wrap_crawl.py
@@ -0,0 +1,184 @@
+import requests as rs
+from bs4 import BeautifulSoup as bsoup
+import re
+from datetime import datetime as count
+import urllib.robotparser as robotparser
+
+
+
+class SiteInfo:
+
+
+	the_date = count.now()
+
+
+	def __init__(self, filename):
+		
+		self.txt_file = filename
+		self.data_from_txt = open(self.txt_file, mode = 'r')
+
+	def create_table(self, name):
+		
+		self.table_name = (
+			name
+			+ '{}'.format(self.the_date.strftime("%Y-%m"))
+			+ ".csv")
+		self.data_table = open(self.table_name, mode = 'w')
+
+
+	def write_table(self, found_data):
+		
+		self.data_table.write(found_data)
+
+
+	@staticmethod
+	def check_url(url):
+		
+		print("The page where crawler starts is: \n" + url + "\n")
+
+
+	def check_robots(self, robots_txt):
+		
+		self.check_robots = robotparser.RobotFileParser()
+		self.check_robots.set_url(robots_txt)
+		self.check_robots.read()
+
+	
+	def close_file(self):
+		
+		self.data_table.close()
+
+
+	def fetch_robot(self, robot_name, url):
+		
+		if self.check_robots.can_fetch(robot_name, url) == True:
+			
+			return(True)
+		else:
+			
+			return(False)
+
+#######################################################################################################################
+
+class TheCrawl:
+
+
+	the_agent = {"User-Agent" : "My_Crawler/0.1 (Windows NT 6.1)", "Contact" : "woj.warych@gmail.com"}
+	the_parser = 'html.parser'
+
+
+	def __init__(self, url):
+		
+
+		self.links = []
+		self.detail_soup = []
+		self.container = []
+		self.reformatted = []
+		self.text = ""
+		self.text_insert = []
+		self.product_spec = []
+		self.url = url
+		crawl = rs.request('GET', url = self.url, headers = self.the_agent)
+		self.soup = bsoup(crawl.content, self.the_parser)
+
+
+	def show_tree(self):
+
+		
+		'''Shows tree of the link'''
+
+		print(self.soup.prettify())
+
+
+	def get_link(self, key, *args, **kwargs):
+
+
+		'''Gets the data from selected link'''
+
+		for link in self.soup.find_all(*args, **kwargs):
+			
+			link = link.get(key)
+			self.links.append(link)
+
+	def get_content(self, SiteInfo):
+
+
+		'''Gets the soup from selected link'''
+
+		for cont, _ in enumerate(self.links):
+			
+			next_link = self.links[cont]
+
+			if SiteInfo.check_robots.can_fetch('*', next_link) == True:
+
+				new_link = rs.get(url = next_link)
+				self.detail_soup.append(bsoup(new_link.content,
+											  self.the_parser))
+			else:
+				pass
+
+
+	def get_details(self, *args, **kwargs):
+
+
+		'''Gets detailed content from the link'''
+
+		for det, _ in enumerate(self.detail_soup):
+
+			parse_text = self.detail_soup[det]
+
+			for cont in parse_text.find_all(*args, **kwargs):
+				self.container.append(cont)
+
+	def insert_to_reformat(self, clean, replacer, position, *args, **kwargs):
+
+
+		'''Insert the data to self.container if forgotten'''
+
+		for det, _ in enumerate(self.detail_soup):
+
+			parse_text = self.detail_soup[det]
+
+			for cont in parse_text.find_all(*args, **kwargs):
+
+				cont = cont.get_text(" ", strip = True)
+				cont = cont.replace(clean, replacer)
+				self.text_insert.append(cont)
+
+		for iteration, _ in enumerate(self.text_insert):
+
+			self.reformatted.insert(position, self.text_insert[iteration])
+			self.reformatted[position - 1:position + 1] = (
+				[' '.join(self.reformatted[position - 1:position + 1])])
+			position += 1
+
+
+	def add_by_regex(self, expression, num, delimiter):
+
+		'''Put to string text from self.reformatted list or NAs if not found'''
+
+		if re.search(expression, self.reformatted[num]):
+			
+			self.text += (
+				(re.search(expression, self.reformatted[num]).group(1))
+				+ delimiter)
+		else:
+			
+			self.text += ("NA" + delimiter)
+
+
+	def clear_text(self):
+
+		self.text = ""
+	
+
+	def reformat(self, clean, replacer):
+
+
+		'''Reformats html body to text'''
+
+		for body, _ in enumerate(self.container):
+			string = self.container[body]
+			string = string.get_text(" ", strip = True)
+			string = string.replace(clean, replacer)
+			self.reformatted.append(string)
